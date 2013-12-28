2a9919e19682eae3bc01f688b35b60895659e7f7
Martin Haesler <martin.haesler@gmail.com>
2013-12-28 08:24:51 +0000

    Added dynamic title for help,about,home.

--- sample_app/spec/requests/static_pages_spec.rb
+++ sample_app/spec/requests/static_pages_spec.rb
@@ -8,11 +8,19 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+    end
   end
   describe "Help page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
+    end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
     end
   end
   describe "About page" do
@@ -20,7 +28,12 @@ describe "StaticPages" do
       visit '/static_pages/about'
       expect(page).to have_content('About')
     end
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+    end
   end
+  
 end
 
 

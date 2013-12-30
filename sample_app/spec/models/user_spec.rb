18a71cdb53c2d87dfb8369d919ebb2a1a6ceb9b4
Martin Haesler <martin.haesler@gmail.com>
2013-12-30 13:24:36 +0000

    Add password-validation and encryption.

--- sample_app/spec/models/user_spec.rb
+++ sample_app/spec/models/user_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+    end
 
   subject { @user }
 
   it { should respond_to(:name)}
   it { should respond_to(:email)}
+  it { should respond_to(:password_digest)}
+  it { should respond_to(:password)}
+  it { should respond_to(:password_confirmation)}
 
   it { should be_valid}
 
@@ -14,6 +20,19 @@ describe User do
     before  { @user.name = " " }
     it { should_not be_valid}
   end
+
+  describe "when password is not present" do
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: " ", password_confirmation: " ")
+  end
+  it { should_not be_valid }
+  end
+  describe "when password doesn't match confirmation" do
+  before { @user.password_confirmation = "mismatch" }
+  it { should_not be_valid }
+  end
+
 
   describe "when email is not present" do
     before { @user.email = " " }
@@ -43,8 +62,18 @@ describe User do
         @user.email = valid_address
         expect(@user).to be_valid
       end
+  end
+    describe "when email address is already taken" do
+    before do
+        user_with_same_email = @user.dup
+        user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+      end
+
+    it { should_not be_valid }
+
+  
     end
   end
-  
   
 end

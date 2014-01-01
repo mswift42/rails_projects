d260d50df11fd89fd6ac134657b82854d4ec62df
Martin Haesler <martin.haesler@gmail.com>
2014-01-01 17:12:33 +0000

    add new_user page

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -3,5 +3,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save
+    else
+      render 'new'
+    end
   end
 end

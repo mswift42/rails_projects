73ab79062e18e477874fbbb5652c57c81ef0f241
Martin Haesler <martin.haesler@gmail.com>
2014-01-01 22:21:06 +0000

    new_user signup.

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -6,11 +6,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
-      # Handle a successful save
+      flash[:success] = "Welcome to the sample app!"
+      redirect_to @user
     else
       render 'new'
     end
+  end
+  def user_params
+    params.require(:user).permit(:name, :email,
+                                 :password, :password_confirmation)
   end
 end

70303aa6b28ad30a8a989a1440d159d9c85b8a94
Martin Haesler <martin.haesler@gmail.com>
2014-01-04 11:56:18 +0000

    Add signin/signout.

--- sample_app/app/helpers/users_helper.rb
+++ sample_app/app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
-  def gravator_for(user)
+  def gravator_for(user, options = {size: 50})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")

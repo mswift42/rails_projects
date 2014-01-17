aaeef04953936d3fb9de7a403d6f9de77b4b7640
Martin Haesler <martin.haesler@gmail.com>
2014-01-17 14:05:31 +0000

    Initial commit.

---
+++ atodolist/app/views/tasks/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @task, :id, :summary, :taskcontent, :scheduled, :created_at, :updated_at
---
+++ atodolist/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Atodolist::Application.config.secret_key_base = 'c0b68c1e630071a2cb8dba5dd9ff42bda42609a847cd4195ee9a35994de57cebded59f476058b4375a67febd6ce54bbabc748c102c517d03463107d86f6e2c6d'
